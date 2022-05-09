9e4bd4c86d989e006849e7ec4c655a8b8696f9fb
wgriff6 <willgv557@gmail.com>
2022-05-08 22:09:15 -0500

    fixed days table

--- server/database/migrations/1651982299071_day_schema.js
+++ server/database/migrations/1651982299071_day_schema.js
@@ -3,24 +3,20 @@
 /** @type {import('@adonisjs/lucid/src/Schema')} */
 const Schema = use('Schema')
 const Database = use('Database')
-//lol I done fucked up somewhere in here >_>
 class DaySchema extends Schema {
   up () {
     this.create('days', (table) => {
-      //table.increments()
-      // table.string('Day', 60)
-      // table.int('Value', 32)
       table.string('Day', 16).notNullable().unsigned().primary()
       table.int('Value', 16).notNullable()
       table.timestamps()
     })
     this.schedule(async (trx) => {
-      const days = ['M', 'T', 'W', 'R', 'F']
+      const days = ['F', 'R', 'W', 'T', 'M']
       await Promise.all(days.map((Day, i) => {
         return Database
           .table('days')
           .transacting(trx)
-          .insert({ Day, Value: (2^i) })
+          .insert({ Day, Value: 2**i })
       }))
     })
   }
